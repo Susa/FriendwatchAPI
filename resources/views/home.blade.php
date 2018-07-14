058c7ddff08d3183a8c5a863683e4d35597b90e6
Ace Jordan <acelumaad@live.com>
2018-07-14 11:55:59 +0800

    updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
 <pre>
-  WatchApp API Server | <a href="{{ route('logout') }}">Logout</a>
+  Friendwatch API Server | <a href="{{ route('logout') }}">Logout</a>
 </pre>
 @endsection
